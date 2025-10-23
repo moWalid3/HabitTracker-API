ef718eb6db09d63199722c4245d81bfb2ecb26f9
Mohamed Walid <mohamedwalidtharwat2020@gmail.com>
2025-10-23 11:14:45 +0300

    Add HabitTracker.UnitTests project and initial test cases

---
+++ HabitTracker.UnitTests/Services/SortMappingProviderTests.cs
@@ -0,0 +1,50 @@
+﻿using HabitTracker.Api.DTOs.Habits;
+using HabitTracker.Api.Entities;
+using HabitTracker.Api.Services.Sorting;
+
+namespace HabitTracker.UnitTests.Services
+{
+    public class SortMappingProviderTests
+    {
+        private readonly SortMappingProvider sortMappingProvider = new([HabitMappings.SortMapping]);
+
+        [Fact]
+        public void Validate_ShouldSuccess_WhenSortFieldIsValid()
+        {
+            // Arrange
+            string sort = "name,type,createdAtUtc";
+
+            // Act
+            bool isMappingValid = sortMappingProvider.ValidateMappings<HabitDto, Habit>(sort);
+
+            // Assert
+            Assert.True(isMappingValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldFail_WhenSortFieldIsNotValidByWrongSeparator()
+        {
+            // Arrange
+            string sort = "name,type-createdAtUtc";
+
+            // Act
+            bool isMappingValid = sortMappingProvider.ValidateMappings<HabitDto, Habit>(sort);
+
+            // Assert
+            Assert.False(isMappingValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldFail_WhenSortFieldIsNotValidByNotExistingField()
+        {
+            // Arrange
+            string sort = "name,type,createdAtUtc,time";
+
+            // Act
+            bool isMappingValid = sortMappingProvider.ValidateMappings<HabitDto, Habit>(sort);
+
+            // Assert
+            Assert.False(isMappingValid);
+        }
+    }
+}
---
+++ HabitTracker.UnitTests/Validators/CreateHabitDtoValidatorTests.cs
@@ -0,0 +1,101 @@
+﻿using FluentValidation.Results;
+using HabitTracker.Api.DTOs.Habits;
+using HabitTracker.Api.Entities;
+
+namespace HabitTracker.UnitTests.Validators
+{
+    public class CreateHabitDtoValidatorTests
+    {
+        private readonly CreateHabitDtoValidator validator = new();
+
+        [Fact]
+        public async Task Validate_ShouldSucceed_WhenInputDtoIsValid()
+        {
+            // Arrange
+            CreateHabitDto dto = new()
+            {
+                Name = "Anything",
+                Type = HabitType.Measurable,
+                Frequency = new FrequencyDto
+                {
+                    Type = FrequencyType.Daily,
+                    TimesPerPeriod = 1
+                },
+                Target = new TargetDto
+                {
+                    Value = 30,
+                    Unit = "minutes"
+                },
+            };
+
+            // Act
+            ValidationResult validationResult = await validator.ValidateAsync(dto);
+
+            // Assert
+            Assert.True(validationResult.IsValid);
+            Assert.Empty(validationResult.Errors);
+        }
+
+        [Fact]
+        public async Task Validate_ShouldFail_WhenNameIsEmptyOrLessThanTwoChars()
+        {
+            // Arrange
+            CreateHabitDto dto = new()
+            {
+                Name = "",
+                Type = HabitType.Measurable,
+                Frequency = new FrequencyDto
+                {
+                    Type = FrequencyType.Daily,
+                    TimesPerPeriod = 1
+                },
+                Target = new TargetDto
+                {
+                    Value = 30,
+                    Unit = "minutes"
+                },
+            };
+
+            // Act
+            ValidationResult validationResult = await validator.ValidateAsync(dto);
+
+            // Assert
+            Assert.False(validationResult.IsValid);
+            Assert.Equal(2, validationResult.Errors.Count);
+            Assert.Equal(nameof(CreateHabitDto.Name), validationResult.Errors.Select(e => e.PropertyName).ElementAt(0));
+            Assert.Equal(nameof(CreateHabitDto.Name), validationResult.Errors.Select(e => e.PropertyName).ElementAt(1));
+        }
+
+        [Fact]
+        public async Task Validate_ShouldFail_WhenTypeIsNotInEnumSoAlsoIsNotCompatibleWithUnit()
+        {
+            // Arrange
+            CreateHabitDto dto = new()
+            {
+                Name = "Anything",
+                Type = (HabitType)3,
+                Frequency = new FrequencyDto
+                {
+                    Type = FrequencyType.Daily,
+                    TimesPerPeriod = 1
+                },
+                Target = new TargetDto
+                {
+                    Value = 30,
+                    Unit = "minutes"
+                },
+            };
+
+            // Act
+            ValidationResult validationResult = await validator.ValidateAsync(dto);
+
+            // Assert
+            Assert.False(validationResult.IsValid);
+            Assert.Equal(2, validationResult.Errors.Count);
+            Assert.Contains(nameof(CreateHabitDto.Type), validationResult.Errors.Select(e => e.PropertyName));
+            Assert.Contains(
+                $"{nameof(CreateHabitDto.Target)}.{nameof(TargetDto.Unit)}",
+                validationResult.Errors.Select(e => e.PropertyName));
+        }
+    }
+}
